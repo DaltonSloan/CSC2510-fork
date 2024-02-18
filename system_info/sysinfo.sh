56f19b25aa3f3465403ae6f5fd05c7d2e155bf4d
SparkleBunz <DaltonW.Sloan@Gmail.com>
2024-02-17 21:50:52 -0600

    Updating sysinfo.sh and system_info.txt.

--- system_info/sysinfo.sh
+++ system_info/sysinfo.sh
@@ -1,4 +1,11 @@
 #! /bin/bash
 
-cat /home/daltonsloan/Desktop/SchoolRepo/CSC2510-Fork/system_info/system_info.txt
-
+cat system_info.txt
+echo "Date and Time" | date +%x\ %T\ %Z
+echo "User: "$(whoami)
+pwd
+echo "System Usage: "
+top -b -n 1 | head -n 5
+echo ""
+echo "Disk Usage: "
+df -h
